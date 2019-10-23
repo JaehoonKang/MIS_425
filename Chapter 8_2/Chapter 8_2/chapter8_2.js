a603360996836362d50432d1845dee1c7f680502
Jaehoon Jay Kang <jhkang1015@gmail.com>
2019-10-23 14:08:25 -0400

    JS 8-2

--- Chapter 8_2/Chapter 8_2/chapter8_2.js
+++ Chapter 8_2/Chapter 8_2/chapter8_2.js
@@ -1,12 +1,26 @@
-console.log('--- create an object ---');
-var myObject = {
-    name: "Scott Geiger",
-    class: "MIS 425",
-    start_time: "6 PM",
-    myArray: ['a', 'b', 'c']
+function myName(first,last) {
+    return 'My Name is: ' + first + ' ' + last;
 }
 
-console.log(myObject);
+console.log('--- function MyName() ---');
+var greeting = myName('Joe', 'Geiger');
+console.log(myName('Scott','Geiger'));
 
-console.log(myObject.name);
-console.log(myObject['class']);
+console.log('--- anonymous function ---');
+var sayHello = function(name) {
+    return 'Hello ' + name;
+}
+
+console.log(sayHello('Joe'));
+
+var complexObj = {
+    first_name: "Scott",
+    last_name: "Geiger",
+    sayHelloAgain: function() {
+        return 'Hello again '+ this.first_name + ' ' + this.last_name;
+    }
+}
+
+console.log(complexObj.sayHelloAgain());
+complexObj.first_name = 'Joe';
+console.log(complexObj.sayHelloAgain());
