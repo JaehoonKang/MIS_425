4840a8920a1219b723cce732add77723f6744165
Jaehoon Jay Kang <jhkang1015@gmail.com>
2019-10-24 13:18:12 -0400

    PreventDefault

--- HW4/HW4/homework4.js
+++ HW4/HW4/homework4.js
@@ -1,1 +1,22 @@
 /* homework4.js - JavaScript for Homework #4 */
+
+
+var myForm = document.getElementById('myForm');
+myForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    var first = document.getElementById('firstName').value;
+    var last = document.getElementById('lastName').value;
+    var day = document.getElementById('dayList').value;
+    var color = document.getElementById('colorList').value;
+
+    //console.log(first);
+    //console.log(last);
+    //console.log(day);
+    //console.log(color);
+
+    
+
+})
+
+
